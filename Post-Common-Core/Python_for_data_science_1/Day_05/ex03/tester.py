4bf4c2a716bae5e36ade0ff8ae108d4a2b10641d
Mlamarcq <mael.lamarcq0302@orange.fr>
2024-10-16 21:44:49 +0200

    Day_05_finished

--- Post-Common-Core/Python_for_data_science_1/Day_05/ex03/tester.py
+++ Post-Common-Core/Python_for_data_science_1/Day_05/ex03/tester.py
@@ -6,7 +6,7 @@ def main():
         student = Student("Edward", "agle")
         print(student)
 
-        student = Student(name = "Edward", surname = "agle", id = "toto")
+        student = Student(name="Edward", surname="agle", id="toto")
         print(student)
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
